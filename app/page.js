8ac54c1d9a4d553ce2a28f641ad0b02d7b618383
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-11-19 00:36:53 +0900

    mongo db 연결 설정

--- app/page.js
+++ app/page.js
@@ -1,4 +1,14 @@
-export default function Home() {
+import { connectDB } from "@/util/database";
+import { MongoClient } from "mongodb";
+
+
+export default async function Home() {
+
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+
   return (
     <div></div>
   );
